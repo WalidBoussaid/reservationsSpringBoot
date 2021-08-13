9c1379bc7660f078136d404bee013de148d21896
WalidBoussaid <walidmeknessi@hotmail.com>
2021-08-13 17:48:09 +0200

    Update templates Show (added artists’ list)

--- src/main/java/be/icc/pid/reservationsSpringBoot/controller/ShowController.java
+++ src/main/java/be/icc/pid/reservationsSpringBoot/controller/ShowController.java
@@ -1,9 +1,14 @@
 package be.icc.pid.reservationsSpringBoot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import be.icc.pid.reservationsSpringBoot.model.ArtistType;
 import be.icc.pid.reservationsSpringBoot.model.Show;
 import be.icc.pid.reservationsSpringBoot.model.ShowService;
+import be.icc.pid.reservationsSpringBoot.model.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,10 +35,24 @@ public class ShowController {
     public String show(Model model, @PathVariable("id") String id) {
         Show show = service.get(id);
 
+        //Récupérer les artistes du spectacle et les grouper par type
+        Map<Type,ArrayList<ArtistType>> collaborateurs = new TreeMap<>();
+
+        for(ArtistType at : show.getArtistTypes()) {
+            if(collaborateurs.get(at.getType()) == null) {
+                collaborateurs.put(at.getType(), new ArrayList<>());
+            }
+
+            collaborateurs.get(at.getType()).add(at);
+        }
+
+        model.addAttribute("collaborateurs", collaborateurs);
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
 
         return "show/show";
     }
 
+
 }
+
