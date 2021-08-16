820a3a18ca1f9854f2060728d4de4f13141e5cc2
WalidBoussaid <walidmeknessi@hotmail.com>
2021-08-16 16:54:55 +0200

    Added UserRepository & UserService

---
+++ src/main/java/be/icc/pid/reservationsSpringBoot/model/UserRepository.java
@@ -0,0 +1,13 @@
+package be.icc.pid.reservationsSpringBoot.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findByLastname(String lastname);
+
+    User findById(long id);
+    User findByLogin(String login);
+    User findByEmail(String email);
+}
